9596674c057d2d13c8dd620947f467667dd23784
Alex <AN270909@falmouth.ac.uk>
2022-05-30 21:05:12 +0100

    camera, controller and inventory

---
+++ Assets/Scripts/ItemInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemInfo : ScriptableObject
+{
+    public string title, description;
+    public Sprite icon;
+}
