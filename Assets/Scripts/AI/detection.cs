fc4ec01cdeeb6686ecfd51166bca0e1603cb7d6a
Alex <AN270909@falmouth.ac.uk>
2022-07-02 02:43:25 +0100

    enemy patrol and detection

---
+++ Assets/Scripts/AI/detection.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class detection : MonoBehaviour
+{
+    public float radius;
+    NavMeshAgent agent;
+    bool seen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        seen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seen = false;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider hitCollider in hitColliders)
+        {
+            if(hitCollider.CompareTag("Player"))
+            {
+                agent.destination = hitCollider.gameObject.transform.position;
+                seen = true;
+            }
+        }
+
+        if(!seen)
+        {
+            //patrol or nav;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/patrol.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour
+{
+    public List<Transform> patrolList = new List<Transform>();
+    public int listPostion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        listPostion = 0;
+    }
+
+    void RunPatrol()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
